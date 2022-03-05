658dd931cde2c10558b0471bbf0cd2be0f2faf48
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-05 09:59:48 +0330

    Complete customize user model

--- account/admin.py
+++ account/admin.py
@@ -10,5 +10,6 @@ from account.models import (
 UserAdmin.fieldsets[1][1]['fields'] = (
     'first_name', 'last_name', 'email', 'avatar'
 )
+UserAdmin.list_display = ("username", "get_avatar", "email", "first_name", "last_name", "is_staff")
 
 admin.site.register(User, UserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.html import format_html
 
 # Create your models here.
 
 
 class User(AbstractUser):
     avatar = models.ImageField(
-        upload_to='account/avatar/', null=True, blank=True)
+        upload_to='account/avatar/', null=True, blank=True, verbose_name='تصویر پروفایل')
+
+    def get_avatar(self):
+        return format_html(f"<img width=75 height=75 style='border-radius: 5px;' src='{self.avatar.url}'>")
+
+    get_avatar.short_description = "تصویر پروفایل"
