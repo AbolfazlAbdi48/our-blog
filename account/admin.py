471aa73deb0a50fc8c2f9b55d2f0aad3e5819ac8
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-09 10:49:28 +0330

    Add: Ticket model & TicketAdmin

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from account.models import (
-    User
+    User,
+    Ticket
 )
 
 # Register your models here
@@ -13,3 +14,12 @@ UserAdmin.fieldsets[1][1]['fields'] = (
 UserAdmin.list_display = ("username", "get_avatar", "email", "first_name", "last_name", "is_staff")
 
 admin.site.register(User, UserAdmin)
+
+
+class TicketAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    search_fields = ('subject', 'message')
+    list_display = ('subject', 'user', 'created')
+    list_filter = ('created',)
+
+admin.site.register(Ticket, TicketAdmin)
--- account/models.py
+++ account/models.py
@@ -13,3 +13,23 @@ class User(AbstractUser):
         return format_html(f"<img width=75 height=75 style='border-radius: 5px;' src='{self.avatar.url}'>")
 
     get_avatar.short_description = "تصویر پروفایل"
+
+
+class Ticket(models.Model):
+    subject = models.CharField(max_length=75, verbose_name='موضوع')
+    user = models.ForeignKey(
+        User, related_name='tickets', on_delete=models.CASCADE, verbose_name='ارسال کننده'
+    )
+    message = models.TextField(verbose_name='پیام')
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name='تاریخ ایجاد'
+    )
+    updated = models.DateTimeField(
+        auto_now=True, verbose_name='تاریخ آخرین ویرایش'
+    )
+    class Meta:
+        verbose_name = 'تیکت'
+        verbose_name_plural = 'تیکت ها'
+
+    def __str__(self):
+        return f"{self.subject}"
