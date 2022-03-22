336d4789192629d5a8d721be99989ff77f708908
Abolfazl Abdi <dev.abolfazlabdi@gmail.com>
2022-03-22 15:55:45 +0430

    Add: jquery & like_article view & save_article view

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from .views import (
     ArticleListView,
     ArticleDetailView,
-    HomePageView
+    HomePageView,
+    like_article,
+    save_article
 )
 
 
 urlpatterns = [
     path('latest', ArticleListView.as_view(), name='article_list'),
     path('<int:pk>/<str:title>', ArticleDetailView.as_view(), name='article_detail'),
-    path('', HomePageView.as_view())
+    path('', HomePageView.as_view(), name='home_page'),
+    path('like/', like_article, name='like_article'),
+    path('save/', save_article, name='save_article'),
 ]
