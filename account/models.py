0a363dab887467ec4b8ffa7151020c430bdca5a2
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-08-03 00:38:11 +0430

    Add: default avatar to User model

--- account/models.py
+++ account/models.py
@@ -8,7 +8,7 @@ from django.utils.html import format_html
 
 class User(AbstractUser):
     avatar = models.ImageField(
-        upload_to='account/avatar/', null=True, blank=True, verbose_name='تصویر پروفایل')
+        upload_to='account/avatar/', null=True, blank=True, verbose_name='تصویر پروفایل', default='defaultpics/profdefault.jpg')
 
     def get_avatar(self):
         return format_html(f"<img width=75 height=75 style='border-radius: 5px;' src='{self.avatar.url}'>")
